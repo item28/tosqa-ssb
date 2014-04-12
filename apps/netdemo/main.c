ba431ffd3fb988ab642ae3cb23d3cc881a421d26
Jean-Claude Wippler <jc@wippler.nl>
2014-04-12 15:18:14 +0200

    added serial shell w/ mem + threads + test cmds

--- apps/netdemo/main.c
+++ apps/netdemo/main.c
@@ -17,17 +17,21 @@
 #include "ch.h"
 #include "hal.h"
 #include "test.h"
-
+#include "shell.h"
+#include "chprintf.h"
 #include "lwipthread.h"
-
 #include "web/web.h"
+
+#define SHELL_WA_SIZE   THD_WA_SIZE(1024)
+#define TEST_WA_SIZE    THD_WA_SIZE(256)
+
+BaseSequentialStream* chp1 = (BaseSequentialStream*) &SD1;
 
 /*
  * Green LED blinker thread, times are in milliseconds.
  */
 static WORKING_AREA(waThread1, 128);
-static msg_t Thread1(void *arg) {
-
+static msg_t Thread1 (void *arg) {
   (void)arg;
   chRegSetThreadName("blinker");
   while (TRUE) {
@@ -36,6 +40,71 @@ static msg_t Thread1(void *arg) {
     palSetPad(GPIO2, GPIO2_RGB1_RED);
     chThdSleepMilliseconds(500);
   }
+  return 0;
+}
+
+// static void cmd_pwrdown(BaseSequentialStream *chp, int argc, char*argv[]){
+//   (void)argv;
+//   if (argc > 0) {
+//     chprintf(chp, "Usage: pwrdown\r\n");
+//     return;
+//   }
+//   chprintf(chp, "Going to power down.\r\n");
+//   chThdSleepMilliseconds(200);
+//   chSysLock();
+//   LPC_SC->PCON |= 0x03; // set PM0 and PM1
+//   __WFI();
+// }
+
+static void cmd_mem(BaseSequentialStream *chp, int argc, char *argv[]) {
+  size_t n, size;
+
+  (void)argv;
+  if (argc > 0) {
+    chprintf(chp, "Usage: mem\r\n");
+    return;
+  }
+  n = chHeapStatus(NULL, &size);
+  chprintf(chp, "core free memory : %u bytes\r\n", chCoreStatus());
+  chprintf(chp, "heap fragments   : %u\r\n", n);
+  chprintf(chp, "heap free total  : %u bytes\r\n", size);
+}
+
+static void cmd_threads(BaseSequentialStream *chp, int argc, char *argv[]) {
+  static const char *states[] = {THD_STATE_NAMES};
+  Thread *tp;
+
+  (void)argv;
+  if (argc > 0) {
+    chprintf(chp, "Usage: threads\r\n");
+    return;
+  }
+  chprintf(chp, "    addr    stack prio refs     state time\r\n");
+  tp = chRegFirstThread();
+  do {
+    chprintf(chp, "%.8lx %.8lx %4lu %4lu %9s %lu\r\n",
+            (uint32_t)tp, (uint32_t)tp->p_ctx.r13,
+            (uint32_t)tp->p_prio, (uint32_t)(tp->p_refs - 1),
+            states[tp->p_state], (uint32_t)tp->p_time);
+    tp = chRegNextThread(tp);
+  } while (tp != NULL);
+}
+
+static void cmd_test(BaseSequentialStream *chp, int argc, char *argv[]) {
+  Thread *tp;
+
+  (void)argv;
+  if (argc > 0) {
+    chprintf(chp, "Usage: test\r\n");
+    return;
+  }
+  tp = chThdCreateFromHeap(NULL, TEST_WA_SIZE, chThdGetPriority(),
+                           TestThread, chp);
+  if (tp == NULL) {
+    chprintf(chp, "out of memory\r\n");
+    return;
+  }
+  chThdWait(tp);
 }
 
 /*
@@ -75,13 +144,30 @@ int main(void) {
   chThdCreateStatic(wa_http_server, sizeof(wa_http_server), NORMALPRIO + 1,
                     http_server, NULL);
 
+  chprintf(chp1, "\n[satmon]\n");
+
+  static const ShellCommand commands[] = {
+    // {"pwrdown", cmd_pwrdown},
+    {"mem", cmd_mem},
+    {"threads", cmd_threads},
+    {"test", cmd_test},
+    {NULL, NULL}
+  };
+
+  static const ShellConfig shell_cfg1 = {
+    (BaseSequentialStream  *)&SD1,
+    commands
+  };
+  /* Shell initialization.*/
+  shellInit();
+  static WORKING_AREA(waShell, SHELL_WA_SIZE);
+  shellCreateStatic(&shell_cfg1, waShell, sizeof(waShell), NORMALPRIO);
+
   /*
    * Normal main() thread activity, in this demo it does nothing except
    * sleeping in a loop and check the button state.
    */
   while (TRUE) {
-    if (palReadPad(GPIO2, GPIO2_PIN12_TO_GND) == 0)
-      TestThread(&SD1);
     chThdSleepMilliseconds(500);
   }
 }
