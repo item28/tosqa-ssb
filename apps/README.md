05c1d773d7bc5229b8b7f04047bd7c49f1c8523c
Jean-Claude Wippler <jc@wippler.nl>
2014-04-14 12:11:12 +0200

    add digipot test

--- apps/README.md
+++ apps/README.md
@@ -2,6 +2,7 @@
 * **canrecv** - copy received CAN data to motor pins
 * **cansend** - periodic send of three ADC readouts
 * **canuid** - send out the LPC11C24's unique device id
+* **digipot** - move the digital vref adjust through its entire range
 * **netdemo** - test application for the AOAA board's LPC1769
 * **pwmstep** - drive the stepper motor from PWM
 * **satdemo** - test the OLED display on the AOAA satellite's LPC11C24
---
+++ apps/digipot/main.cpp
@@ -0,0 +1,51 @@
+// ssb11 stepper - test stepper motor pulsing
+// jcw, 2014-04-10
+
+#include "ch.h"
+#include "hal.h"
+
+// keep a LED blinking at 2 Hz in the background
+static WORKING_AREA(waBlinkTh, 64);
+static msg_t BlinkTh(void*) {
+  for (;;) {
+    chThdSleepMilliseconds(250);
+    palTogglePad(GPIO1, GPIO1_LED1);
+  }
+  return 0;
+}
+
+int main () {
+  halInit();
+  chSysInit();
+  
+  // brief red flash on startup
+  palTogglePad(GPIO0, GPIO0_LED2);
+  chThdSleepMilliseconds(100);
+  palTogglePad(GPIO0, GPIO0_LED2);
+
+  // launch the blinker background thread
+  chThdCreateStatic(waBlinkTh, sizeof(waBlinkTh), NORMALPRIO, BlinkTh, NULL);
+
+  // send 64 pulses to move the digipot, then reverse direction
+  for (;;) {
+    palTogglePad(GPIO1, GPIO1_DIGIPOT_UD);
+    chThdSleepMilliseconds(1);
+    palClearPad(GPIO3, GPIO3_DIGIPOT_CS);
+    chThdSleepMilliseconds(1);
+    for (int i = 0; i < 64; ++i) {
+      palTogglePad(GPIO1, GPIO1_DIGIPOT_UD);
+      chThdSleepMilliseconds(1);
+      palTogglePad(GPIO1, GPIO1_DIGIPOT_UD);
+      chThdSleepMilliseconds(98);
+    }
+    palSetPad(GPIO3, GPIO3_DIGIPOT_CS);
+    chThdSleepMilliseconds(1);
+
+    // switch direction and LEDs
+    palTogglePad(GPIO1, GPIO1_MOTOR_DIR);
+    palTogglePad(GPIO1, GPIO1_LED1);
+    palTogglePad(GPIO0, GPIO0_LED2);
+  }
+
+  return 0;
+}
